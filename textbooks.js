cfb18c9267d2642fd44d0779b3be581b3a5edda3
Sean Doran <sdoran35@gmail.com>
2020-08-18 15:46:51 -0500

    new textbook files

---
+++ textbooks.js
@@ -0,0 +1,14 @@
+const bookFileTypes = Object.freeze({
+    PDF:'PDF',
+    EPUB:'EPUB',
+    DOC:'DOC',
+    HTML:'HTML'
+});
+
+const bookList = [
+    {
+        'book_title':'',
+        'book_year':'',
+        'book_file_type': bookFileTypes.PDF
+    }
+]
